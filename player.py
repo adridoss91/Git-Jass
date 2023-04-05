6d207e92dfdf80bc25f2f7c3a3caaaa7bd751d3e
adridoss91 <124768156+adridoss91@users.noreply.github.com>
2023-04-05 22:25:29 +0200

    Create Up Until Play Function

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-############SETUP############
+############ SETUP ############
 import random
 
 
@@ -6,27 +6,32 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.cards = []
-        self.teammate = None
         self.order = None
         self.points = 0
     
     def __repr__(self):
         print("This is a Jass-Player named {0}. They are in a team with {1}.".format(self.name, self.teammate))
-    
-    def set_teammate(self, teammate):
-        self.teammate = teammate
+
     
     def show_cards(self):
         print("""
         {0} has the following cards:"
         """.format(self.name))
         for card in self.cards:
-            print(card.color, card.name)
+            print(card.full_name)
     
-    def play_card(self, card):
-        self.cards.pop(card)
-        print(card)
-        return card
+    def play_card(self, table):
+        print(f"{self.name}'s turn:")
+        for i in range(len(self.cards)):
+            print(f"{i} {self.cards[i].full_name}")
+        card_to_play = int(input("Select Card to Play by Number: "))
+        if card_to_play not in range((len(self.cards))-1):
+            print(f"Please select a number between 0 and {len(self.cards) - 1}")
+        else:
+            played_card = self.cards.pop(card_to_play)
+            table.append(played_card)
+            print(f"{self.name} played {played_card.full_name}")
+
 
 class Cards:
     def __init__(self, name, color):
@@ -34,6 +39,7 @@ class Cards:
         self.color = color
         self.might = None
         self.value = None
+        self.full_name = self.color + " " + self.name
     
     def get_name(self):
         return self.name
@@ -78,41 +84,67 @@ class Cards:
                 self.might = 1
                 self.value = 0
 
-card_colors = ["Rose", "Schilte", "Schelle", "Eichle"]
-card_names = ["Ass", "Koenig", "Ober", "Under", "Banner", "Neun", "Acht", "Sieben", "Sechs"]
-card_stack = []
 
 
-#########Create Cards##############
-for color in card_colors:
-    for name in card_names:
-        card_stack.append(Cards(name, color))
 
-for card in card_stack:
-    card.set_might_value()
+######### Create Cards ##############
+def create_cards():
+    card_colors = ["Rose", "Schilte", "Schelle", "Eichle"]
+    card_names = ["Ass", "Koenig", "Ober", "Under", "Banner", "Neun", "Acht", "Sieben", "Sechs"]
+    card_stack = []
+    for color in card_colors:
+        for name in card_names:
+            card_stack.append(Cards(name, color))
 
-
-##########Create Players##############
-player_1 = Player("Xaver")
-player_2 = Player("Annemarie")
-player_3 = Player("Alfred")
-player_4 = Player("Elisabeth")
-
-
-##########Shuffle Cards###############
-random.shuffle(card_stack)
+    for card in card_stack:
+        card.set_might_value()
+    return card_stack
 
 
-##########Distribute Cards###############
-while len(card_stack) > 0:
-    player_1.cards.append(card_stack.pop(0))
-    player_2.cards.append(card_stack.pop(0))
-    player_3.cards.append(card_stack.pop(0))
-    player_4.cards.append(card_stack.pop(0))
+########## Create Players ##############
+def create_player(player):
+    return Player(player)
 
 
-############GAME PLAY###############
-player_1.show_cards()
-player_2.show_cards()
-player_3.show_cards()
-player_4.show_cards()
+
+########## Shuffle Cards ###############
+def shuffle_cards(card_stack):
+    random.shuffle(card_stack)
+
+
+########## Distribute Cards ###############
+def distribute_cards(player_1, player_2, player_3, player_4, card_stack):
+    while len(card_stack) > 0:
+        player_1.cards.append(card_stack.pop(0))
+        player_2.cards.append(card_stack.pop(0))
+        player_3.cards.append(card_stack.pop(0))
+        player_4.cards.append(card_stack.pop(0))
+
+
+############ PLAY ROUND ###############
+def play_round(player_1, player_2, player_3, player_4):
+    table = []
+    player_1.play_card(table)
+    player_2.play_card(table)
+    player_3.play_card(table)
+    player_4.play_card(table)
+    print(" ")
+    for card in table:
+        print(card.full_name)
+
+
+############ COUNT POINTS ##############
+
+
+
+############   TESTING ZONE    #################
+card_stack = create_cards()
+player_1 = create_player("Annemarie")
+player_2 = create_player("Elisabeth")
+player_3 = create_player("Xaver")
+player_4 = create_player("Alfred")
+shuffle_cards(card_stack)
+distribute_cards(player_1, player_2, player_3, player_4, card_stack)
+play_round(player_1, player_2, player_3, player_4)
+play_round(player_1, player_2, player_3, player_4)
+play_round(player_1, player_2, player_3, player_4)
